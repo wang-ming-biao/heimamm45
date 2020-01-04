0b15c3e9781bbde25062a7d89902260bc4f0b786
wang-ming-biao <1340921277@qq.com>
2020-01-04 10:34:19 +0800

    完成登录基本功能,开始注册功能

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -34,11 +34,9 @@
               <el-input placeholder="请输入右侧的验证码" prefix-icon="el-icon-key" v-model="ruleForm.code"></el-input>
             </el-col>
             <el-col :span="6" class="code-col">
-              <img class="cdInput-img"  :src="ruleForm.codeUrl" alt />
+              <img class="cdInput-img" @click="codeChange" :src="codeUrl" alt />
             </el-col>
           </el-row>
-
-          <!-- <div class="codeBox"></div> -->
         </el-form-item>
       </el-form>
       <el-checkbox class="Privacy" v-model="ruleForm.checked">
@@ -46,14 +44,39 @@
         <el-link type="primary">用户协议</el-link>和
         <el-link type="primary">隐私条款</el-link>
       </el-checkbox>
-      <el-button type="primary" class="my-btn">登录</el-button>
-      <el-button type="primary" class="my-btn">注册</el-button>
+      <el-button type="primary" class="my-btn" @click="loginBtn">登录</el-button>
+      <el-button type="primary" class="my-btn" @click="dialogFormVisible = true">注册</el-button>
     </div>
-    <img class="img" src="../../assets/login_banner_ele.png" alt />
+    <!-- 遮罩层 -->
+<el-dialog title="用户注册" :visible.sync="dialogFormVisible" class="maskTitle">
+  <!-- 用户头像上传 -->
+  <el-upload
+  class="avatar-uploader"
+  action="https://jsonplaceholder.typicode.com/posts/"
+  :show-file-list="false"
+  :on-success="handleAvatarSuccess"
+  :before-upload="beforeAvatarUpload">
+  <img v-if="imageUrl" :src="imageUrl" class="avatar">
+  <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+</el-upload>
+<!-- 用户资料表单 -->
+  <el-form :model="form">
+    <el-form-item label="活动名称" :label-width="formLabelWidth">
+      <el-input v-model="form.name" autocomplete="off"></el-input>
+    </el-form-item>
+  </el-form>
+  <div slot="footer" class="dialog-footer">
+    <el-button @click="dialogFormVisible = false">取 消</el-button>
+    <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+  </div>
+</el-dialog>
+    <img class="img"  src="../../assets/login_banner_ele.png" alt />
   </div>
 </template>
 
 <script>
+// 导入axios
+import axios from 'axios'
 // 当输入框失去焦点时调用这里的方法
 const validatePhone = (rule, value, callback) => {
   if (value === "") {
@@ -72,41 +95,119 @@ const validatePhone = (rule, value, callback) => {
 export default {
   data() {
     return {
+      codeUrl: process.env.VUE_APP_BASEURL + '/captcha?type=login' ,
+      imageUrl: '',//用户头像上传
       ruleForm: {
         name: "", //输入框绑定
         password: "", //密码框
         code: "", //验证码
         checked: false ,//多选框选定
-        codeUrl: process.env.VUE_APP_BASEURL + '/captcha?type=login' ,
       },
       rules: {
         // 手机号码验证
         name: [{ required: true, validator: validatePhone, trigger: "blur" }],
         password: [
-          { required: true, message: "请输入密码", trigger: "change" },
-          { min: 6, max: 18, message: "长度在 6 到 18 个字符", trigger: "blur" }
+          { required: true, message: "请输入密码", trigger: "blur" },
+          { min: 6, max: 18, message: "长度在 6 到 18 个字符", trigger: "change" }
         ],
         code: [
           { required: true, message: "请输入验证码", trigger: "blur" },
           { min: 4, max: 4, message: "长度必须为4", trigger: "change" }
         ]
-      }
+      },
+      // 遮罩层代码
+       gridData: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }],
+        dialogTableVisible: false,
+        dialogFormVisible: false,
+        form: {
+          name: '',
+          region: '',
+          date1: '',
+          date2: '',
+          delivery: false,
+          type: [],
+          resource: '',
+          desc: ''
+        },
+        formLabelWidth: '120px'
     };
   },
   methods: {
-    submitForm(formName) {
-      this.$refs[formName].validate(valid => {
-        if (valid) {
-          alert("submit!");
-        } else {
-          // console.log('error submit!!');
-          return false;
-        }
-      });
+    // 验证码变化事件
+    codeChange(){
+      this.codeUrl = process.env.VUE_APP_BASEURL + '/captcha?type=login&' + Math.random()*999;
     },
-    resetForm(formName) {
-      this.$refs[formName].resetFields();
-    }
+    // 登录按钮
+    loginBtn(){
+      if (this.ruleForm.checked == true) {
+        axios({
+        method:"post",
+        url:process.env.VUE_APP_BASEURL + "/login",
+        withCredentials:true,
+        data:{
+              phone: this.ruleForm.name,
+              password: this.ruleForm.password,
+              code:this.ruleForm.code
+        }
+      }).then(res=>{
+        if (res.data.code == 200) {
+          return this.$message({
+          message: "登录成功",
+          type: 'warning'
+        });
+        }else {
+          window.console.log(res)
+        return this.$message({
+          message: res.data.message,
+          type: 'warning'
+        });
+        }
+      })
+      }else{
+      this.Agreement();
+      return
+      }
+      
+    },
+    // 错误提示
+    Agreement() {
+        this.$message({
+          message: '请确认用户协议之后进行登录',
+          type: 'warning'
+        });
+      },
+    // 用户头像上传
+     handleAvatarSuccess(res, file) {
+        this.imageUrl = URL.createObjectURL(file.raw);
+      },
+      beforeAvatarUpload(file) {
+        const isJPG = file.type === 'image/jpeg';
+        const isLt2M = file.size / 1024 / 1024 < 2;
+
+        if (!isJPG) {
+          this.$message.error('上传头像图片只能是 JPG 格式!');
+        }
+        if (!isLt2M) {
+          this.$message.error('上传头像图片大小不能超过 2MB!');
+        }
+        return isJPG && isLt2M;
+      }
   }
 };
 </script>
@@ -124,6 +225,34 @@ export default {
     rgba(1, 198, 250, 1),
     rgba(20, 147, 250, 1)
   );
+
+  // 用户头像上传
+  .avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
+  .avatar-uploader .el-upload:hover {
+    border-color: #409EFF;
+  }
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+  }
+  .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
+  }
+  .el-dialog__title {
+    text-align: center;
+  } 
   // 左侧登录框大盒子
   .left {
     width: 478px;
@@ -139,10 +268,10 @@ export default {
       margin-bottom: 25px;
     }
     // 验证码框
-    // .cdInput-img {
-    //   width: 100px;
-    //   height: 42px;
-    // }
+    .cdInput-img {
+      width: 100%;
+      height: 42px;
+    }
     // 顶部标题盒子
     .title-box {
       display: flex;
